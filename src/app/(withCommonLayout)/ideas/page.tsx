9b96666382a8affa11b9f4dd323ee73bc2757bcc
Md Ahosanul Islam <ahosanul81@gmail.com>
2025-07-06 10:49:35 +0600

    fixed: a route undefined

--- src/app/(withCommonLayout)/ideas/page.tsx
+++ src/app/(withCommonLayout)/ideas/page.tsx
@@ -1,10 +1,6 @@
-import { getAllIdea } from "@/services/idea";
 import React from "react";
 
 export default async function IdeaPage() {
-  const data = await getAllIdea();
-  console.log(data);
-
   return (
     <div>
       <h1 className="text-black">Get all idea </h1>
